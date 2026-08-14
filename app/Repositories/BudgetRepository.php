4c3bc29366399303d21e3436ba72cff6c36d445d
Rusty <decolongonrusty23@gmail.com>
2026-08-14 22:42:37 +0800

    update repo and service for budget

--- app/Repositories/BudgetRepository.php
+++ app/Repositories/BudgetRepository.php
@@ -2,13 +2,22 @@
 
 namespace App\Repositories;
 
-class BudgetRepository
+use App\Interfaces\BudgetInterface;
+use App\Models\Budget;
+use Illuminate\Support\Facades\Auth;
+
+class BudgetRepository implements BudgetInterface
 {
-    /**
-     * Create a new class instance.
-     */
-    public function __construct()
+   
+    public function createBudget(array $data): Budget
     {
-        //
+        $data['author_id'] = Auth::id();
+        return Budget::create($data);
+    }
+
+    public function updateBudget(Budget $budget, array $data): Budget
+    {
+        $budget->update($data);
+        return $budget;
     }
 }
--- app/Services/BudgetService.php
+++ app/Services/BudgetService.php
@@ -2,13 +2,23 @@
 
 namespace App\Services;
 
+use App\Interfaces\BudgetInterface;
+use App\Models\Budget;
+
 class BudgetService
 {
     /**
      * Create a new class instance.
      */
-    public function __construct()
+    public function __construct(private BudgetInterface $budgetRepository){}
+   
+    public function createBudget(array $data): Budget
     {
-        //
+        return $this->budgetRepository->createBudget($data);
+    }
+
+    public function updateBudget(Budget $budget, array $data): Budget
+    {
+        return $this->budgetRepository->updateBudget($budget, $data);
     }
 }
