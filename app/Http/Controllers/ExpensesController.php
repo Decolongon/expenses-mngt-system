7ce33d81c1b37de94ded799c3c61654ac8b19215
Rusty <decolongonrusty23@gmail.com>
2026-08-17 20:54:29 +0800

    finished the controller for expenses

--- app/Http/Controllers/ExpensesController.php
+++ app/Http/Controllers/ExpensesController.php
@@ -2,20 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Concerns\HasFlashMessage;
+use App\Http\Requests\Expense\CreateExpenseRequest;
+use App\Http\Requests\Expense\UpdateExpenseRequest;
 use App\Models\Expense;
+use App\Services\CategoryService;
 use App\Services\ExpensesService;
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Inertia\Inertia;
 
 class ExpensesController extends Controller
 {
+    use HasFlashMessage;
 
-    public function __construct(private ExpensesService $expensesService){}
+    public function __construct(private ExpensesService $expensesService, private CategoryService $categoryService){}
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
-        //
+        return Inertia::render('expenses/expenses-index', [
+            'expenses' => $this->expensesService->getAllExpenses(),
+        ]);
     }
 
     /**
@@ -23,15 +31,19 @@ class ExpensesController extends Controller
      */
     public function create()
     {
-        //
+        return Inertia::render('expenses/expenses-create',[
+            'categories' => $this->categoryService->getCategories(),
+        ]);
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(CreateExpenseRequest $request)
     {
-        //
+        $this->expensesService->createExpenses($request->validated());
+        $this->successMessage('Expense created successfully.');
+        return to_route('expenses.index');
     }
 
     /**
@@ -47,15 +59,23 @@ class ExpensesController extends Controller
      */
     public function edit(Expense $expense)
     {
-        //
+        abort_if($expense->author_id !== Auth::id(), 403, 'You are not authorized to edit this expense.');
+        $expense->load(['category:id,name']);
+        return Inertia::render('expenses/expenses-edit',[
+            'categories' => $this->categoryService->getCategories(),
+            'expense' => $expense
+        ]);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Expense $expense)
+    public function update(UpdateExpenseRequest $request, Expense $expense)
     {
-        //
+        abort_if($expense->author_id !== Auth::id(), 403, 'You are not authorized to update this expense.');
+        $this->expensesService->updateExpenses($expense, $request->validated());
+        $this->successMessage('Expense updated successfully.');
+        return to_route('expenses.index');
     }
 
     /**
@@ -63,6 +83,9 @@ class ExpensesController extends Controller
      */
     public function destroy(Expense $expense)
     {
-        //
+        abort_if($expense->author_id !== Auth::id(), 403, 'You are not authorized to delete this expense.');
+        $expense->delete();
+        $this->successMessage('Expense deleted successfully.');
+        return to_route('expenses.index');
     }
 }
