8cbae1e7f8e11f54439a52a721af36855eb80b20
Rusty <decolongonrusty23@gmail.com>
2026-08-19 19:56:41 +0800

    add encrypt history  for security

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::inertia('/', 'Welcome')->name('home');
 
-Route::middleware(['auth', 'verified'])->group(function () {
+Route::middleware(['auth', 'verified',Inertia\Middleware\EncryptHistory::class])->group(function () {
     Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
 
     Route::resource('categories', CategoryController::class)->except('show');
